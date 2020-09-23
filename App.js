185a27113cf853da93017a5d9319b4383dae7281
201810824Hyunsomi <62291578+201810824Hyunsomi@users.noreply.github.com>
2020-09-23 21:42:11 +0900

    count-up event

---
+++ App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  state = {
+    count:0
+  };
+
+  countUp = () => {
+    this.setState({
+      count: this.state.count +1
+    });
+  };
+  render() {
+    return (
+    <div className="App">
+      <div>{this.state.count}</div>;
+      <button onClick={this.countUp}>count Me!</button>
+      </div>
+    );
+  }
+}
+
+export default App;

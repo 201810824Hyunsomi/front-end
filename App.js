e2fa961a02cef164f2c41acf55ee958cd1e30ca8
201810824Hyunsomi <62291578+201810824Hyunsomi@users.noreply.github.com>
2020-09-25 12:45:18 +0900

    kakaomap -react view

--- App.js
+++ App.js
@@ -1,23 +1,36 @@
-import React, { Component } from 'react';
+/*global kakao*/
+import React, { Component, KakaoMap } from "react";
+import styled from "styled-components";
 
-class App extends Component {
-  state = {
-    count:0
-  };
+class MapContent extends Component {
+  componentDidMount() {
+    const script = document.createElement("script");
+    script.async = true;
+    script.src =
+      "https://dapi.kakao.com/v2/maps/sdk.js?appkey=07489920b7a2167f937a0011ff16dd53&autoload=false";
+    document.head.appendChild(script);
+    script.onload = () => {
+      kakao.maps.load(() => {
+        let container = document.getElementById("Mymap");
+        let options = {
+          center: new kakao.maps.LatLng(37.60226275383353, 126.95533647295382),
+          level: 7
+        };
 
-  countUp = () => {
-    this.setState({
-      count: this.state.count +1
-    });
-  };
+        const map = new window.kakao.maps.Map(container, options);
+     
+      });
+    };
+  }
+
   render() {
-    return (
-    <div className="App">
-      <div>{this.state.count}</div>;
-      <button onClick={this.countUp}>count Me!</button>
-      </div>
-    );
+    return <MapContents id="Mymap"></MapContents>;
   }
 }
 
-export default App;
+const MapContents = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+export default MapContent;
